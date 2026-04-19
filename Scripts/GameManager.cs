2cdc1fc4114b76ceb00dd16ad7a6b36aee73f727
Michael Kurz <kurzmichael02@gmail.com>
2026-04-19 18:07:15 +0200

    removed dead _score field from gamemanager
    gamemanager.addscore() wasn't called anywhere – player keeps its own score. just confusing noise.

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -4,14 +4,8 @@ using Godot;
 // Right now just swaps to the gameplay music when the level loads.
 public partial class GameManager : Node2D
 {
-	private int _score = 0;
-
 	public override void _Ready()
 	{
 		SoundManager.Instance.SwitchMusic(SoundManager.Instance.GameMusic);
-	}
-	public void AddScore(int amount)
-	{
-		_score += amount;
 	}
 }
